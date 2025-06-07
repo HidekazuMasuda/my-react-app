9c71468e4d2899cfec81d222796ba74ce8b794ec
HidekazuMasuda <sana.php@gmail.com>
2025-06-07 10:48:41 +0900

    トップページに名前を入れられるようにする

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="welcome-section">
+          <h1>ようこそ{name ? `${name}さん` : ''}！</h1>
+          <div className="input-section">
+            <label htmlFor="nameInput">お名前を入力してください：</label>
+            <input
+              id="nameInput"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="お名前"
+              className="name-input"
+            />
+          </div>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
